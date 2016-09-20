97d5f3f46611a4d4289c1db3af2f8600fc8e87e2
U028879 <U028879@P0130801.cajarural.gcr>
2016-09-20 15:13:37 +0200

    vcz

--- REP_RURALVIA_REST/src/com/rsi/rvia/rest/Cards.java
+++ REP_RURALVIA_REST/src/com/rsi/rvia/rest/Cards.java
@@ -14,38 +14,43 @@ import javax.ws.rs.core.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.rsi.rvia.rest.client.OperationManager;
-import com.rsi.rvia.rest.session.SessionRviaData;
 
 /** Clase que responde a las peticiones REST para las acciones sobre una coleccion de tarjetas */
 @Path("/cards")
 public class Cards
 {
-	private static Logger			pLog	= LoggerFactory.getLogger(Cards.class);
+	private static Logger pLog = LoggerFactory.getLogger(Cards.class);
 
-	/** Obtiene el listado completo de tarjetas de un usuario
+	/**
+	 * Obtiene el listado completo de tarjetas de un usuario
 	 * 
 	 * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
-	 * @throws Exception */
+	 * @throws Exception
+	 */
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public Response getAllUserCards(@Context HttpServletRequest pRequest,@Context UriInfo pUriInfo, String strData) throws Exception
+	public Response getAllUserCards(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
+			throws Exception
 	{
 		Response pReturn = OperationManager.proccesDataFromRvia(pRequest, pUriInfo, strData, MediaType.APPLICATION_XHTML_XML_TYPE);
 		return pReturn;
 	}
-	
+
 	@POST
 	@Path("/{card}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response getCard(@Context HttpServletRequest pRequest,@Context UriInfo pUriInfo, String strData) throws Exception
+	public Response getCard(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
+			throws Exception
 	{
 		Response pReturn = OperationManager.proccesDataFromRvia(pRequest, pUriInfo, strData, MediaType.APPLICATION_JSON_TYPE);
 		return pReturn;
 	}
-	
-	/** Fija el estado de bloqueo de una tarjeta
+
+	/**
+	 * Fija el estado de bloqueo de una tarjeta
 	 * 
-	 * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas */
+	 * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
+	 */
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
