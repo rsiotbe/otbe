bf127d18a7a75f77df792d9108b7b4fd971af714
David García Sánchez <davidgarsan@gmail.com>
2016-10-04 16:36:09 -0700

    Fix test

--- test/com/rsi/rvia/rest/client/SaveExitHierarchyTest.java
+++ test/com/rsi/rvia/rest/client/SaveExitHierarchyTest.java
@@ -2,30 +2,25 @@ package com.rsi.rvia.rest.client;
 
 import static org.junit.Assert.assertTrue;
 import org.json.JSONObject;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import com.rsi.BaseTest;
 
 public class SaveExitHierarchyTest extends BaseTest
 {
-   @Before
-   public void setUp() throws Exception
-   {
-   }
-
-   @After
-   public void tearDown() throws Exception
-   {
-   }
-
    @Test
    public void testProcess() throws Exception
    {
       JSONObject jsonData = new JSONObject("{response:{foo:'bar'}}");
       int idMiq = 0;
       String strMethod = "method";
-      SaveExitHierarchy.process(jsonData, idMiq, strMethod);
-      assertTrue(true);
+      try
+      {
+         SaveExitHierarchy.process(jsonData, idMiq, strMethod);
+         assertTrue(true);
+      }
+      catch (Exception e)
+      {
+         assertTrue("testProcess: Error", false);
+      }
    }
 }
