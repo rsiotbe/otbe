6478518765229a80dd701a0c24602d045a891277
Kike <U020964@cajarural.com>
2016-10-05 10:07:48 +0200

    arreglos

--- src/com/rsi/rvia/rest/LoanSimulators.java
+++ src/com/rsi/rvia/rest/LoanSimulators.java
@@ -3,6 +3,7 @@ package com.rsi.rvia.rest;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,6 +42,35 @@ public class LoanSimulators
 	@Consumes({ MediaType.APPLICATION_XHTML_XML, MediaType.TEXT_HTML, MediaType.APPLICATION_FORM_URLENCODED,
 			"application/x-ms-application" })
 	public Response getAllSimulators(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo,
+			@PathParam("entidad") String strBankName) throws Exception
+	{
+		pLog.info("Se recibe una peticion de pagina inical de todos los simuladores sin idioma específico");
+		pLog.info("entidad: " + strBankName);
+		Response pReturn = OperationManager.processDataFromSimulators(pRequest, pUriInfo, strBankName, null, MediaType.TEXT_HTML_TYPE);
+		pLog.info("Se devuelve la respuesta final al usuario");
+		return pReturn;
+	}
+
+	@POST
+	@Path("{entidad}/{idioma}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getConfigSimulators(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo,
+			@PathParam("entidad") String strBankName, @PathParam("idioma") String strlanguage) throws Exception
+	{
+		pLog.info("Se recibe una peticion de pagina inical de todos los simuladores");
+		pLog.info("entidad: " + strBankName);
+		pLog.info("idioma: " + strlanguage);
+		Response pReturn = OperationManager.processDataFromSimulators(pRequest, pUriInfo, strBankName, strlanguage, MediaType.TEXT_HTML_TYPE);
+		pLog.info("Se devuelve la respuesta final al usuario");
+		return pReturn;
+	}
+
+	@POST
+	@Path("{entidad}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getConfigSimulators(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo,
 			@PathParam("entidad") String strBankName) throws Exception
 	{
 		pLog.info("Se recibe una peticion de pagina inical de todos los simuladores sin idioma específico");
