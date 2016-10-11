31e70b5f4753c00b53d2b9072198597366bcb95b
Kike <U020964@cajarural.com>
2016-10-11 08:52:24 +0200

    test de isum

--- test/com/rsi/isum/IsumValidationTest.java
+++ test/com/rsi/isum/IsumValidationTest.java
@@ -2,7 +2,6 @@ package com.rsi.isum;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import com.rsi.TestBase;
@@ -11,43 +10,41 @@ import com.rsi.rvia.rest.session.SessionRviaData;
 
 public class IsumValidationTest extends TestBase
 {
-   @Mock
-   SessionRviaData session;
+	@Mock
+	SessionRviaData	session;
 
-   @Test
-   public void testIsValidServiceOk() throws Exception
-   {
-      final String ISSUM_PROFILE = "0198TS00";
-      final String ISSUM_SERVICE_ID = "PROXY_RVIA_REST_1022";
-      when(session.getIsumUserProfile()).thenReturn(ISSUM_PROFILE);
-      when(session.getIsumServiceId()).thenReturn(ISSUM_SERVICE_ID);
-      boolean isValid = IsumValidation.IsValidService(session);
-      assertTrue("IsValidServiceThrowsISUMExceptionTest: No es una sesión válida", isValid);
-   }
+	@Test
+	public void testIsValidServiceOk() throws Exception
+	{
+		final String ISSUM_PROFILE = "0198TS00";
+		final String ISSUM_SERVICE_ID = "PROXY_RVIA_REST_1022";
+		when(session.getIsumUserProfile()).thenReturn(ISSUM_PROFILE);
+		when(session.getIsumServiceId()).thenReturn(ISSUM_SERVICE_ID);
+		boolean isValid = IsumValidation.IsValidService(session);
+		assertTrue("IsValidServiceThrowsISUMExceptionTest: No es una sesión válida", isValid);
+	}
 
-   @Ignore
-   @Test
-   public void testIsValidServiceNotOk() throws Exception
-   {
-      boolean isValid = IsumValidation.IsValidService(session);
-      assertTrue("IsValidServiceThrowsISUMExceptionTest: Es una sesión inválida", !isValid);
-   }
+	@Test
+	public void testIsValidServiceNotOk() throws Exception
+	{
+		boolean isValid = IsumValidation.IsValidService(session);
+		assertTrue("IsValidServiceThrowsISUMExceptionTest: Es una sesión inválida", !isValid);
+	}
 
-   @Ignore
-   @Test
-   public void testIsValidServiceThrowsISUMException()
-   {
-      try
-      {
-         IsumValidation.IsValidService(null);
-      }
-      catch (ISUMException e)
-      {
-         assertTrue(true);
-      }
-      catch (Exception e)
-      {
-         assertTrue("IsValidServiceThrowsISUMExceptionTest: No lanza excepción tipo ISUMException", false);
-      }
-   }
+	@Test
+	public void testIsValidServiceThrowsISUMException()
+	{
+		try
+		{
+			IsumValidation.IsValidService(null);
+		}
+		catch (ISUMException e)
+		{
+			assertTrue(true);
+		}
+		catch (Exception e)
+		{
+			assertTrue("IsValidServiceThrowsISUMExceptionTest: No lanza excepción tipo ISUMException", false);
+		}
+	}
 }
