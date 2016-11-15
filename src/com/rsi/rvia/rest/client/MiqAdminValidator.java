d6a5e42b5dadac24d4fd12712141d0ab6317ef46
Leopoldo Martinez Araujo <xenhaust@gmail.com>
2016-11-15 16:12:52 +0100

    Fin de securizado

--- src/com/rsi/rvia/rest/client/MiqAdminValidator.java
+++ src/com/rsi/rvia/rest/client/MiqAdminValidator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.http.HttpEntity;
@@ -14,17 +15,38 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.jose4j.jwt.MalformedClaimException;
 import org.jose4j.lang.JoseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MiqAdminValidator
 {
+    private static Logger pLog = LoggerFactory.getLogger(MiqAdminValidator.class);
+
     public static void adminIn(HttpServletRequest pRequest, HttpServletResponse pResponse)
             throws MalformedClaimException, NoSuchAlgorithmException, InvalidKeySpecException, JoseException,
             IOException
     {
         HashMap<String, String> pParamsToInject = new HashMap<String, String>();
         String JWT = pRequest.getParameter("admTk");
+        Cookie cookies[];
         String uri = pRequest.getRequestURI();
         String validos = "u020885u020976u028879";
+        cookies = pRequest.getCookies();
+        if (cookies != null)
+        {
+            if (JWT == null)
+            {
+                for (int i = 0; i < cookies.length; i++)
+                {
+                    String name = cookies[i].getName();
+                    if ("admTk".equals(name))
+                    {
+                        JWT = cookies[i].getValue();
+                        break;
+                    }
+                }
+            }
+        }
         if (JWT == null)
         {
             doURL(pResponse, "/api/access/adminLogin.jsp?requesturi=" + uri);
@@ -84,7 +106,7 @@ public class MiqAdminValidator
                     + "\"  name=\"admTk\">                                                           "
                     + "                    <p><label>User:</label> <input type=\"text\" placeholder=\"Usuario\" name=\"user\" value=\"\"></p>           "
                     + "                    <p><label>Pass:</label> <input type=\"password\" placeholder=\"Password\" name=\"pass\" value=\"\"></p>      "
-                    + "                    <p><input type=\"submit\" value=\"Take me inside\"></p>                                                      "
+                    + "                    <p><input type=\"submit\" value=\"Take me inside\"></p><br><br>                                              "
                     + "                </form>                                                                                                          "
                     + "            </div>                                                                                                               "
                     + "            <div class=\"c1-n c4-12-post\">&nbsp;</div>                                                                          "
@@ -133,6 +155,10 @@ public class MiqAdminValidator
             HashMap<String, String> claims = new HashMap<String, String>();
             claims.put("user", strUser);
             JWT = ManageJWToken.generateJWT(claims, "admTk");
+            Cookie cookie = new Cookie("admTk", JWT);
+            cookie.setMaxAge(60 * 60 * 24); // 1 hour
+            cookie.setPath("/api");
+            pResponse.addCookie(cookie);
             return makeHTML("", "", strRequestUri, strRequestUri, JWT, "", true);
         }
         return makeHTML("", "", "", strRequestUri, JWT, "Donde crees que vas pájaro?<br><br>Que va a ser que aquí no puedes entrar sin el santo y seña !!", false);
