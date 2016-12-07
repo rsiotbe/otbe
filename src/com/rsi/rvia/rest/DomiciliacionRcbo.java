29b4eac61e25ed5578078eec6352ea4e65aec3a4
Kike <U020964@cajarural.com>
2016-12-07 17:00:58 +0100

    nuevo template

--- src/com/rsi/rvia/rest/DomiciliacionRcbo.java
+++ src/com/rsi/rvia/rest/DomiciliacionRcbo.java
@@ -15,41 +15,41 @@ import com.rsi.rvia.rest.client.OperationManager;
 @Path("/domiciliations")
 public class DomiciliacionRcbo
 {
-    private static Logger pLog = LoggerFactory.getLogger(DomiciliacionRcbo.class);
+	private static Logger	pLog	= LoggerFactory.getLogger(DomiciliacionRcbo.class);
 
-    /**
-     * Obtiene el listado de cuentas del cliente.
-     * 
-     * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
-     * @throws Exception
-     */
-    @GET
-    @Path("/contracts")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response listaDeContratos(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
-            throws Exception
-    {
-        // return Response.ok("Lista de contratos").build();
-        Response pReturn = OperationManager.processDataFromRvia(pRequest, pUriInfo, "{}", MediaType.APPLICATION_JSON_TYPE);
-        pLog.info("Lista de contratos");
-        return pReturn;
-    }
+	/**
+	 * Obtiene el listado de cuentas del cliente.
+	 * 
+	 * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
+	 * @throws Exception
+	 */
+	@GET
+	@Path("/contracts")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listaDeContratos(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
+			throws Exception
+	{
+		// return Response.ok("Lista de contratos").build();
+		Response pReturn = OperationManager.processDataFromRvia(pRequest, pUriInfo, "{}", MediaType.APPLICATION_JSON_TYPE);
+		pLog.info("Lista de contratos");
+		return pReturn;
+	}
 
-    /**
-     * Obtiene el listado de emisores de decibos para una cuenta.
-     * 
-     * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
-     * @throws Exception
-     */
-    @GET
-    @Path("/transmitter/{idContract}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response listaDeEmisores(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
-            throws Exception
-    {
-        // return Response.ok("Lista de contratos").build();
-        Response pReturn = OperationManager.processDataFromRvia(pRequest, pUriInfo, "{}", MediaType.APPLICATION_JSON_TYPE);
-        pLog.info("Lista de contratos");
-        return pReturn;
-    }
+	/**
+	 * Obtiene el listado de emisores de asociados a una cuenta.
+	 * 
+	 * @return Objeto que contiene la respuesta y en caso positivo se adjunta el listado de tarjetas
+	 * @throws Exception
+	 */
+	@GET
+	@Path("/transmitter/{idContract}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listaDeEmisores(@Context HttpServletRequest pRequest, @Context UriInfo pUriInfo, String strData)
+			throws Exception
+	{
+		// return Response.ok("Lista de contratos").build();
+		Response pReturn = OperationManager.processDataFromRvia(pRequest, pUriInfo, "{}", MediaType.APPLICATION_JSON_TYPE);
+		pLog.info("Lista de contratos");
+		return pReturn;
+	}
 }
