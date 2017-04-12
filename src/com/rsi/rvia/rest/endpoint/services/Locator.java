265ba675d976826598f97a4a018f2cb850ef78fa
Jorge Regidor Martin <jorgeregidor@gmail.com>
2017-04-12 17:10:52 +0200

    Localizador correcto y terminado, sin los import innecesarios y con el fichero guardado

--- src/com/rsi/rvia/rest/endpoint/services/Locator.java
+++ src/com/rsi/rvia/rest/endpoint/services/Locator.java
@@ -1,5 +1,6 @@
 package com.rsi.rvia.rest.endpoint.services;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,8 +8,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.rsi.rvia.rest.client.OperationManager;
 
 
