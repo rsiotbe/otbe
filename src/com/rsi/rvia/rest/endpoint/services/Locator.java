32f084aed6d49ee812760df94ad549cc6653a5a7
Jorge Regidor Martin <jorgeregidor@gmail.com>
2017-04-12 17:05:43 +0200

    Localizador correcto y terminado, sin los import innecesarios

--- src/com/rsi/rvia/rest/endpoint/services/Locator.java
+++ src/com/rsi/rvia/rest/endpoint/services/Locator.java
@@ -1,26 +1,16 @@
 package com.rsi.rvia.rest.endpoint.services;
 
-import java.util.Iterator;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.rsi.rvia.rest.client.OperationManager;
-import com.rsi.rvia.rest.response.RviaRestResponse;
+
 
 @Path("/locator")
 public class Locator
