18a732dbaee27fb66a1024d452d29860cee45ce8
falarcon_rsi <f_a_escolar_servext_rsi@cajarural.com>
2017-06-12 11:33:10 +0200

    Cambio de anotacion de @POST a @GET

--- src/com/rsi/rvia/rest/endpoint/ruralvia/tarjetas/TarjetaCredito.java
+++ src/com/rsi/rvia/rest/endpoint/ruralvia/tarjetas/TarjetaCredito.java
@@ -126,7 +126,7 @@ public class TarjetaCredito
      *            the uri info
      * @return the datos persona
      */
-    @POST
+    @GET
     @Path("/scoring/formdata")
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({ MediaType.APPLICATION_JSON })
